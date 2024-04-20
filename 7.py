96d7d9914c22fd53570ed800b3301323e759ac8d
NessyMoonlight <159246841+NessyMoonlight@users.noreply.github.com>
2024-04-20 12:02:04 +0700

    ~

--- 7.py
+++ 7.py
@@ -1,16 +1,21 @@
 for send in range(1000, 9999 + 1):
-    if len(set(str(send))) == len(str(send)):
+    send = str(send)
+    if len(set(send)) == len(send):
+
         for more in range(1000, 9999 + 1):
-            if len(set(str(more))) == len(str(more)) and \
-                    str(send)[1]==str(more)[-1] and \
-                    set(str(send)[0]+str(send)[2:]) != set(str(more)[:3]+str(send)[-1]):
-                    money = str(send + more)
-                    if money[0:2] == str(more)[0:2] and \
-                            money[2] == str(send)[2] and \
-                            money[-2] == str(more)[-1] and \
-                            money[-1] != set(str(send)) and money[-1] != set(str(more))\
+            more = str(more)
+            if len(set(more)) == len(more) and \
+                    send[1]==more[-1] and \
+                    more[1] not in set(send) and more[2] not in set(send) and \
+                    more[0] not in set(send):
+
+                    money = str(int(send) + int(more))
+                    if money[0:2] == more[0:2] and \
+                            money[2] == send[2] and \
+                            money[-2] == more[-1] and \
+                            money[-1] != set(send) and money[-1] != set(more)\
                             and len(set(money)) == len(money)\
-                            and money[-1] not in set(str(send))\
-                            and money[-1] not in set(str(more)):
+                            and money[-1] not in set(send)\
+                            and money[-1] not in set(more):
                                 print(f"{send} + {more} = {money}")
                                 break
